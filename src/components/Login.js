1f76bf9ae9f310cfdf8ccb9bbf1a5e71ea9f9a4d
unknown <y-schmidt>
2022-06-01 15:43:48 +0300

    merge commits

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,26 @@
+function Loginput () {
+    let loginbtn=document.getElementById("loginbtn")
+document.getElementById("btn").addEventListener('click',()=>{
+let input=document.createElement("input")
+input.type="text"
+input.id="logintext"
+loginbtn.appendChild(input)
+let inputpass=document.createElement("input")
+inputpass.type="password"
+inputpass.id="loginpassword"
+loginbtn.appendChild(input)
+})
+}
+
+
 function Login () {
+
+
     return(
-        <button>login</button>
-    )
+       <div id="loginbtn">
+        <button id="btn" onClick={Loginput} >login</button>
+
+    </div>)
 }
 
 export default Login
